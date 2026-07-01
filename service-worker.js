34f8bb3c41fc8e71e161043de7a98d1edac288b5
uniconsulta <marcosdalleprane2@gmail.com>
2026-07-01 01:02:27 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,70 @@
-const CACHE_NAME = "consultaedu-suporte-v6";
+const CACHE_NAME = "consultaedu-suporte-v7";
+
+const STATIC_FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
 self.addEventListener("install", event => {
   self.skipWaiting();
 
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./app.js",
-        "./manifest.json"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_FILES))
   );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
+    caches.keys().then(keys =>
+      Promise.all(
         keys
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
-      );
-    })
+      )
+    )
   );
 
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
+  const request = event.request;
+  const url = new URL(request.url);
+
+  // Nunca cachear chamadas POST
+  if (request.method !== "GET") {
+    event.respondWith(fetch(request));
+    return;
+  }
+
+  // Nunca cachear APIs externas
+  if (
+    url.hostname.includes("script.google.com") ||
+    url.hostname.includes("script.googleusercontent.com") ||
+    url.hostname.includes("workers.dev") ||
+    url.hostname.includes("api.telegram.org")
+  ) {
+    event.respondWith(fetch(request));
+    return;
+  }
+
+  // Arquivos do app: tenta rede primeiro, cache depois
   event.respondWith(
-    caches.match(event.request).then(resposta => {
-      return resposta || fetch(event.request);
-    })
+    fetch(request)
+      .then(response => {
+        const clone = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(request, clone);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(request))
   );
 });
