008887cc93eea69111ac43a0f6d3c4374bc28f1c
uniconsulta <marcosdalleprane2@gmail.com>
2026-06-30 22:40:09 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,21 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("consultaedu-suporte-v1").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./app.js",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
 
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resposta => {
+      return resposta || fetch(event.request);
+    })
+  );
+});
